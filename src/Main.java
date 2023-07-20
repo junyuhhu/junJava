27664f3f635c74a6a436f4138a843bb7a115751d
junyuhhu <131837386+junyuhhu@users.noreply.github.com>
2023-07-20 14:12:31 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         //git
+        //git2
         System.out.println("111");
         System.out.println("dev");
         System.out.println("master");
