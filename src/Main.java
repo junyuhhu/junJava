5ed4c125e472b5bafcea81b6749a23dfe361415c
junyu <junyuhhu@163.com>
2023-07-20 15:57:33 +0800

    seven

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        //git
-        //git2
-        System.out.println("111");
-        System.out.println("dev");
-        System.out.println("master");
-        System.out.println("git");
-        System.out.println("bendi");
-        System.out.println("hhh");
-        System.out.println(22);
-        System.out.println(11);
-        //bendi
+
     }
 }
